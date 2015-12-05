ced617f7cd99d318042acc5ebd6c38ee3167e0de
Sergei Dolgov <dolgovs@gmail.com>
2015-12-05 18:07:34 +0100

    Solve 5.2

--- 05.hs
+++ 05.hs
@@ -1,27 +1,54 @@
 import Data.List (isInfixOf)
+import qualified Data.Map.Strict as Map
 
 containsNOf :: Eq a=> Int -> [a] -> [a] -> Bool
 containsNOf n ys xs = (>=n) $ sum $ map length $ map (\y -> filter (==y) xs) ys
 
-contains3Vowels :: String -> Bool
-contains3Vowels = containsNOf 3 "aeiou"
-
-containsADouble :: Eq a => [a] -> Bool
-containsADouble xs = any (uncurry (==)) $ zip xs (tail xs)
-
 doesNotContainAnyOf :: Eq a => [[a]] -> [a] -> Bool
 doesNotContainAnyOf yss xs = all id $ map (\ys -> not $ ys `isInfixOf` xs) yss
 
-doesNotContainForbidenStrings :: String -> Bool
-doesNotContainForbidenStrings = doesNotContainAnyOf ["ab", "cd", "pq", "xy"]
+containsADoubleSeparatedBy :: Eq a => Int -> [a] -> Bool
+containsADoubleSeparatedBy n xs = any (uncurry (==)) $ zip xs (drop (n+1) xs)
+
+containsNonOverlappingSublistsOfLength :: Ord a => Int -> [a] -> Bool
+containsNonOverlappingSublistsOfLength n =
+  (>0) . length . filter (>=n) . fartherstOccurences . multipleOccurences . listsOfIndexes . toMapOfListsOfIndexes . subListsOfLength n
+  where
+    subListsOfLength :: Int -> [a] -> [[a]]
+    subListsOfLength n xs = takeWhile ((==n) . length) $ map (take n) $ map (\m -> drop m xs) [0..]
+
+    toMapOfListsOfIndexes :: Ord a => [[a]] -> Map.Map [a] [Int]
+    toMapOfListsOfIndexes ss = Map.fromListWith (++) $ zip ss (map (\x -> [x]) [0..])
+
+    listsOfIndexes :: Map.Map [a] [Int] -> [[Int]]
+    listsOfIndexes = Map.elems
+
+    multipleOccurences :: [[a]] -> [[a]]
+    multipleOccurences = filter ((>1) . length) 
+
+    fartherstOccurences :: [[Int]] -> [Int]
+    fartherstOccurences = map (\xs -> abs $ (last xs) - (head xs))
 
 satisfiesAllConditions :: [String -> Bool] -> String -> Bool
 satisfiesAllConditions fs ss = all (==True) $ map (\f -> f ss) fs
 
-isNice :: String -> Bool
-isNice = satisfiesAllConditions [contains3Vowels, containsADouble, doesNotContainForbidenStrings]
-
-countNice :: [String] -> Int
-countNice = length . filter (==True) . map isNice 
+countNice :: (String -> Bool) -> [String] -> Int
+countNice f = length . filter (==True) . map f
   
-main = interact $ (++"\n") . show . countNice . lines
+isNice :: String -> Bool
+isNice = satisfiesAllConditions
+         [
+           containsNOf 3 "aeiou",
+           containsADoubleSeparatedBy 0,
+           doesNotContainAnyOf ["ab", "cd", "pq", "xy"]
+         ]
+
+isNice2 :: String -> Bool
+isNice2 = satisfiesAllConditions
+         [
+           containsADoubleSeparatedBy 1,
+           containsNonOverlappingSublistsOfLength 2
+         ]
+
+-- main = interact $ (++"\n") . show . countNice isNice . lines
+main = interact $ (++"\n") . show . countNice isNice2 . lines

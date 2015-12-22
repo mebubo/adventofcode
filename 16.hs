0852d05fb3cfd9a16c9f951c288d7832a4752fe4
Sergei Dolgov <dolgovs@gmail.com>
2015-12-22 21:29:03 +0100

    Solve 16.2

--- 16.hs
+++ 16.hs
@@ -2,17 +2,31 @@ module Day16 where
 
 import Data.Map (fromList, Map, lookup)
 
-tickerTape :: Map String Int
-tickerTape = fromList [("children", 3),
-                       ("cats", 7),
-                       ("samoyeds", 2),
-                       ("pomeranians", 3),
-                       ("akitas", 0),
-                       ("vizslas", 0),
-                       ("goldfish", 5),
-                       ("trees", 3),
-                       ("cars", 2),
-                       ("perfumes", 1)
+type Tape = Map String (Int -> Bool)
+
+tickerTape1 :: Tape
+tickerTape1 = fromList [("children", (==3)),
+                       ("cats", (==7)),
+                       ("samoyeds", (==2)),
+                       ("pomeranians", (==3)),
+                       ("akitas", (==0)),
+                       ("vizslas", (==0)),
+                       ("goldfish", (==5)),
+                       ("trees", (==3)),
+                       ("cars", (==2)),
+                       ("perfumes", (==1))
+                      ]
+
+tickerTape2 = fromList [("children", (==3)),
+                       ("cats", (>7)),
+                       ("samoyeds", (==2)),
+                       ("pomeranians", (<3)),
+                       ("akitas", (==0)),
+                       ("vizslas", (==0)),
+                       ("goldfish", (<5)),
+                       ("trees", (>3)),
+                       ("cars", (==2)),
+                       ("perfumes", (==1))
                       ]
 
 readAunt :: String -> [(String, Int)]
@@ -24,17 +38,19 @@ readAunt =
     toPairs [] = []
     toPairs (x:y:ys) = (x, read y) : toPairs ys
 
-isMatchingAunt :: [(String, Int)] -> Bool
-isMatchingAunt = all f
+isMatchingAunt :: Tape -> [(String, Int)] -> Bool
+isMatchingAunt t = all f
                  where
                    f :: (String, Int) -> Bool
-                   f (s, i) = case Data.Map.lookup s tickerTape of
+                   f (s, i) = case Data.Map.lookup s t of
                               Nothing -> True
-                              Just i' -> i == i'
+                              Just f -> f i
 
 showAunt :: [(String, Int)] -> String
 showAunt = show . snd . head
 
 main = do
   input <- getContents
-  print $ (map showAunt) . (filter isMatchingAunt) . (map readAunt) . lines $ input
+  let solve tape = map showAunt . filter (isMatchingAunt tape) . map readAunt . lines
+  print $ solve tickerTape1 input
+  print $ solve tickerTape2 input

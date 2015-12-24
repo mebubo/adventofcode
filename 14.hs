c1e15e6a996cc4a0607c085ff08be788dfaf1cde
Sergei Dolgov <dolgovs@gmail.com>
2015-12-24 12:56:55 +0100

    Solve 14.2

--- 14.hs
+++ 14.hs
@@ -1,6 +1,10 @@
 module Day14 where
 
 import Text.Regex.Posix
+import Data.List (transpose, findIndices)
+
+raceDuration :: Int
+raceDuration = 2503
 
 type Reindeer = (Int, Int, Int)
 
@@ -20,8 +24,25 @@ distance time traveled isRacing deer@(speed, timeRacing, timeResting)
   where d = speed * timeRacing
 
 solution :: [Reindeer] -> Int
-solution = maximum . map (distance 2503 0 True)
+solution = maximum . map (distance raceDuration 0 True)
+
+movements :: Reindeer -> [Int]
+movements (speed, timeRacing, timeResting) = concat . repeat $ replicate timeRacing speed ++ replicate timeResting 0
+
+positions :: Reindeer -> [Int]
+positions = scanl1 (+) . movements
+
+pointsForPosition :: [Int] -> [Int]
+pointsForPosition xs = map (oneOrZero $ maximum xs) xs
+  where oneOrZero a b = if a == b then 1 else 0
+
+positionsToPoints :: [[Int]] -> [[Int]]
+positionsToPoints = transpose . map pointsForPosition . transpose
+
+solution2 :: [Reindeer] -> Int
+solution2 = maximum . map sum . positionsToPoints . map (take raceDuration . positions)
 
 main = do
   input <- getContents
-  print $ solution $ map parseReindeer $ lines input
+  print . solution . map parseReindeer . lines $ input
+  print . solution2 . map parseReindeer . lines $ input

74ff1b5a03aaaf35b721a01c949384fff347b1a6
Sergei Dolgov <dolgovs@gmail.com>
2016-01-14 22:16:34 +0100

    Try to implement applySpell

--- 22.hs
+++ 22.hs
@@ -3,16 +3,18 @@ data Boss = Boss { bossHp :: Int, damage :: Int } deriving Show
 
 data Spell = MagicMissile Int Int
            | Drain Int Int Int
-           | Shield Int Int Int
-           | Poison Int Int Int
-           | Recharge Int Int Int deriving Show
+           | Shield Int Int
+           | Poison Int Int
+           | Recharge Int Int deriving Show
 
-spells :: [Spell]
-spells = [ MagicMissile 53 4
-         , Drain 73 2 2
-         , Shield 113 6 7
-         , Poison 173 6 3
-         , Recharge 229 5 101
+data TimedSpell = TimedSpell Int Spell
+
+spells :: [TimedSpell]
+spells = [ TimedSpell 1 $ MagicMissile 53 4
+         , TimedSpell 1 $ Drain 73 2 2
+         , TimedSpell 6 $ Shield 113 7
+         , TimedSpell 6 $ Poison 173 3
+         , TimedSpell 5 $ Recharge 229 101
          ]
 
 player :: Player
@@ -20,3 +22,6 @@ player = Player 50 500 []
 
 boss :: Boss
 boss = Boss 51 9
+
+applySpell :: (Player -> Boss -> TimedSpell) -> (Player, Boss, TimedSpell)
+applySpell (p, Boss hp damage, TimedSpell t (MagicMissile m d)) = (p, Boss (hp - d) damage, TimedSpell (t-1) $ MagicMissile m d)

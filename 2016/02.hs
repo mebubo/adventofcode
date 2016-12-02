828d103a37abc8c622f61923b81fad7c181d3ab6
Sergei Dolgov <dolgovs@gmail.com>
2016-12-02 14:01:04 +0100

    02.2

--- 2016/02.hs
+++ 2016/02.hs
@@ -1,38 +1,56 @@
+import Numeric (showHex)
+
 data Instruction = U | R | D | L deriving Read
 
-data Coord = Coord Int Int
+data Coord = Coord Int Int deriving Show
 
-type Key = Int
+type Key = String
 
-move :: Instruction -> Coord -> Coord
-move U (Coord r c) = Coord (r-1) c
-move R (Coord r c) = Coord r (c+1)
-move D (Coord r c) = Coord (r+1) c
-move L (Coord r c) = Coord r (c-1)
+type Move = Coord -> Instruction -> Coord
 
-contain :: Coord -> Coord
-contain (Coord x y) = Coord (contain' x) (contain' y)
-    where contain' a | a < 0 = 0
-                    | a > 2 = 2
-                    | otherwise = a
+move :: Move
+move (Coord r c) U = Coord (r-1) c
+move (Coord r c) R = Coord r (c+1)
+move (Coord r c) D = Coord (r+1) c
+move (Coord r c) L = Coord r (c-1)
 
-containedMove :: Coord -> Instruction -> Coord
-containedMove c i = contain $ move i c
+getKey1 :: Coord -> Key
+getKey1 (Coord r c) = show $ 3*r + c + 1
 
-getKey :: Coord -> Key
-getKey (Coord r c) = 3*r + c + 1
+getKey2 :: Coord -> Key
+getKey2 (Coord r c) = showHex (5*r + r' + c - 1)  ""
+    where r' = sum . take r $ [-3, -1, -1, -3]
 
-decodeKey :: Coord -> [Instruction] -> Key
-decodeKey c = getKey . foldl containedMove c
+findCoord :: Move -> Coord -> [Instruction] -> Coord
+findCoord m c = foldl m c
 
 readInstructions :: String -> [Instruction]
 readInstructions = map (read . (:[]))
 
-start :: Coord
-start = Coord 1 1
+start1 :: Coord
+start1 = Coord 1 1
+
+start2 :: Coord
+start2 = Coord 2 0
+
+within1 :: Coord -> Bool
+within1 (Coord r c) = 0 <= r && r <=2 && 0 <= c && c <= 2
+
+within2 :: Coord -> Bool
+within2 (Coord r c) | r == 0 || r == 4 = c == 2
+                    | r == 1 || r == 3 = 1 <= c && c <= 3
+                    | r == 2 = 0 <= c && c <= 4
+                    | otherwise = False
+
+containedMove :: (Coord -> Bool) -> Coord -> Instruction -> Coord
+containedMove within c i =
+    let candidate = move c i
+    in
+        if within candidate then candidate else c
 
 main :: IO ()
 main = do
     input <- getContents
     let inst = map readInstructions . lines $ input
-    print $ concatMap (show . decodeKey start) inst
+    print $ concatMap (getKey1 . findCoord (containedMove within1) start1) inst
+    print $ concatMap (getKey2 . findCoord (containedMove within2) start2) inst

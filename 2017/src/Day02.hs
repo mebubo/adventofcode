8a398cd29157afb40c5c7a29464bde8fccdfa928
Sergei Dolgov <dolgovs@gmail.com>
2017-12-03 07:24:51 +0100

    02.2

--- 2017/src/Day02.hs
+++ 2017/src/Day02.hs
@@ -1,14 +1,31 @@
 module Day02 where
 
-minMax :: Ord a => [a] -> (a, a)
-minMax = (,) <$> minimum <*> maximum
+type Input = [[Int]]
+type Output = Int
+
+readInput :: IO Input
+readInput = map (map read . words) . lines <$> readFile "input/02.input"
 
 minMaxDiff :: (Ord a, Num a) => [a] -> a
-minMaxDiff xs = b - a
-    where (a, b) = minMax xs
+minMaxDiff = (-) <$> maximum <*> minimum
+
+solve1 :: Input -> Output
+solve1 = sum . map minMaxDiff
+
+allPairs :: [a] -> [(a, a)]
+allPairs xs = (,) <$> xs <*> xs
+
+divisible :: Integral a => (a, a) -> Bool
+divisible (a, b) = a /= b && a `mod` b == 0
+
+divide :: [Int] -> Int
+divide = uncurry div . head . filter divisible . allPairs
+
+solve2 :: Input -> Output
+solve2 = sum . map divide
 
 main :: IO ()
 main = do
-    d <- map (map read . words) . lines <$> readFile "input/02.input"
-    let diffs = map minMaxDiff d
-    print $ sum diffs
+    d <- readInput
+    print $ solve1 d
+    print $ solve2 d

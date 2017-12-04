7d1548ef9e0a938d1ee7c7942e962939b7578767
Sergei Dolgov <dolgovs@gmail.com>
2017-12-04 06:14:33 +0100

    Siplify

--- 2017/src/Day04.hs
+++ 2017/src/Day04.hs
@@ -9,10 +9,10 @@ readInput :: IO Input
 readInput = map words . lines <$> readFile "input/04.input"
 
 solve1:: Input -> Result
-solve1= length . filter (all (==1)) . map (map length . group . sort)
+solve1 = length . filter (all (==1) . map length . group . sort)
 
 solve2 :: Input -> Result
-solve2 = length . filter (all (==1)) . map (map length . group . sort . map sort)
+solve2 = solve1 . map (map sort)
 
 main :: IO ()
 main = do

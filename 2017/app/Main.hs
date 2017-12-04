d8e3b980ca9bc60c926d527c322a49ee348264d8
Sergei Dolgov <dolgovs@gmail.com>
2017-12-04 06:13:22 +0100

    04

--- 2017/app/Main.hs
+++ 2017/app/Main.hs
@@ -5,12 +5,14 @@ import System.Environment (getArgs)
 import qualified Day01
 import qualified Day02
 import qualified Day03
+import qualified Day04
 
 mains :: [IO ()]
 mains =
     [ Day01.main
     , Day02.main
     , Day03.main
+    , Day04.main
     ]
 
 main :: IO ()
---
+++ 2017/src/Day04.hs
@@ -0,0 +1,21 @@
+module Day04 where
+
+import Data.List (group, sort)
+
+type Input = [[String]]
+type Result = Int
+
+readInput :: IO Input
+readInput = map words . lines <$> readFile "input/04.input"
+
+solve1:: Input -> Result
+solve1= length . filter (all (==1)) . map (map length . group . sort)
+
+solve2 :: Input -> Result
+solve2 = length . filter (all (==1)) . map (map length . group . sort . map sort)
+
+main :: IO ()
+main = do
+    i <- readInput
+    print $ solve1 i
+    print $ solve2 i

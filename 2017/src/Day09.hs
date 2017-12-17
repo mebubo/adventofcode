90b2dff9733887d94836953dbdffc134a1b097e9
Sergei Dolgov <dolgovs@gmail.com>
2017-12-17 13:13:45 +0100

    09.2

--- 2017/src/Day09.hs
+++ 2017/src/Day09.hs
@@ -2,44 +2,50 @@ module Day09 where
 
 import Text.Megaparsec
 import Text.Megaparsec.Char
-import Data.Tree (Tree(..), Forest, levels)
-import Data.Maybe (catMaybes)
+import Data.Tree (Tree(..), Forest, levels, flatten)
+import Data.Maybe (catMaybes, isNothing)
+import Control.Monad (join)
 
 type Parser = Parsec () String
 
-garbage :: Parser ()
+type Garbage = Maybe String
+
+garbage :: Parser Garbage
 garbage = do
     char '<'
-    many $ escaped <|> nonClose
+    x <- catMaybes <$> many (escaped <|> nonClosed)
     char '>'
-    return ()
+    return $ Just x
     where
-        escaped = char '!' *> anyChar
-        nonClose = noneOf ">"
+        escaped = Nothing <$ (char '!' *> anyChar)
+        nonClosed = Just <$> noneOf ">"
 
-groups :: Parser (Forest ())
+groups :: Parser (Forest Garbage)
 groups = do
     char '{'
     gs <- group `sepBy` char ','
     char '}'
-    return $ catMaybes gs
+    return gs
 
-group :: Parser (Maybe (Tree ()))
-group = Just <$> (Node () <$> groups) <|> (garbage *> pure Nothing)
-
-rootGroup :: Parser (Tree ())
-rootGroup = do
-    Just g <- group
-    return g
-
-score :: Tree a -> Int
-score t = sum $ zipWith f (levels t) [1..]
+group :: Parser (Tree Garbage)
+group = grps <|> garb
     where
-        f :: [a] -> Int -> Int
-        f as n = n * length as
+        grps = Node Nothing <$> groups
+        garb = f <$> garbage
+        f g = Node g []
+
+score1 :: Tree Garbage -> Int
+score1 t = sum $ zipWith f (levels t) [1..]
+    where
+        f :: [Garbage] -> Int -> Int
+        f as n = n * length (filter isNothing as)
+
+score2 :: Tree Garbage -> Int
+score2 = length . join . catMaybes . flatten
 
 main :: IO ()
 main = do
     c <- readFile "input/09.input"
-    let Right i = parse rootGroup "" c
-    print $ score i
+    let Right i = parse group "" c
+    print $ score1 i
+    print $ score2 i

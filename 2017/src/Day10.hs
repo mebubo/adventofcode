0bde914891ae919401f88218d758a09436bf52fc
Sergei Dolgov <dolgovs@gmail.com>
2017-12-17 19:35:38 +0100

    10.2

--- 2017/src/Day10.hs
+++ 2017/src/Day10.hs
@@ -1,12 +1,23 @@
 module Day10 where
 
-import Data.List.Split (wordsBy)
+import Data.List.Split (wordsBy, chunksOf)
 import Data.Vector (toList, fromList, (//))
+import Data.Char (ord)
+import Data.Bits (xor)
+import Control.Monad (join)
+import Text.Printf (printf)
 
+type RawInput = String
 type Input = [Int]
 
-readInput :: IO Input
-readInput = (read <$>) . wordsBy (==',') <$> readFile "input/10.input"
+readInput :: IO RawInput
+readInput = readFile "input/10.input"
+
+getInput :: RawInput -> Input
+getInput = (read <$>) . wordsBy (==',')
+
+getInput2 :: RawInput -> Input
+getInput2 = (ord <$>) . filter (/= '\n')
 
 type State = ([Int], Int, Int)
 
@@ -31,7 +42,25 @@ steps ns = foldl (flip (.)) id $ step <$> ns
 initial :: State
 initial = ([0..255], 0, 0)
 
+fst' :: State -> [Int]
+fst' (a, _, _) = a
+
+suffix = [17, 31, 73, 47, 23]
+
+steps2 :: [Int] -> State -> State
+steps2 = steps . concat . replicate 64 . (++ suffix)
+
+
+postProcess :: [Int] -> String
+postProcess xs = toString =<< xorAll <$> chunksOf 16 xs
+    where
+        xorAll :: [Int] -> Int
+        xorAll = foldl xor 0
+        toString :: Int -> String
+        toString = printf "%02x"
+
 main :: IO ()
 main = do
     i <- readInput
-    print $ product $ take 2 $ (\(a, _, _) -> a) $ steps i initial
+    print . product . take 2 . fst' . flip steps initial . getInput $ i
+    print . postProcess . fst' . flip steps2 initial . getInput2 $ i
